3501185df8ee97d0fc10f279dd1f7aa3d50bb5dd
Larry Schiefer <larry.schiefer@hiqes.com>
2014-12-04 16:34:08 -0600

    Interim commit with moving to dynamic fragments.  Partially converted to a MainActivity and StatusFragment.  Still refactoring.

--- app/src/main/java/com/intel/yamba/StatusFragment.java
+++ app/src/main/java/com/intel/yamba/StatusFragment.java
@@ -1,18 +1,20 @@
 package com.intel.yamba;
 
 import android.app.Activity;
+import android.app.Fragment;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.AsyncTask;
-import android.preference.PreferenceManager;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -21,76 +23,86 @@ import android.widget.Toast;
 import com.marakana.android.yamba.clientlib.YambaClient;
 import com.marakana.android.yamba.clientlib.YambaClientException;
 
+public class StatusFragment extends Fragment implements TextWatcher, View.OnClickListener {
+    public static final String          TAG = "StatusFragment";
 
-public class StatusActivity extends Activity implements TextWatcher, View.OnClickListener {
+    /**
+     * This interface is required for all hosting Activities.  Implement it. Be Happy.
+     */
+    public interface StatusRequestCallback  {
+        public void needSettings();
+    }
+
     private static final int            MAX_STATUS_LEN = 140;
     private static final int            WARNING_STATUS_LEN = 10;
 
-    private TextView            mRemainText;
-    private EditText            mStatus;
-    private Button              mSubmitBtn;
+    private TextView mRemainText;
+    private EditText mStatus;
+    private Button mSubmitBtn;
     private Button              mSettingsBtn;
     private int                 mDefaultTextColor;
     private YambaSubmitterTask  mTmpTask;
+    private StatusRequestCallback   mCallback;
+
+    public StatusFragment() {
+    }
+
+    public static StatusFragment createInstance() {
+        return new StatusFragment();
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_status);
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
 
-        mStatus = (EditText)findViewById(R.id.status_text);
-        mRemainText = (TextView)findViewById(R.id.status_remain);
+        try {
+            mCallback = (StatusRequestCallback)activity;
+        } catch (ClassCastException e) {
+            Log.d(TAG, "Invalid host activity, no callback included");
+            throw e;//new ClassCastException(e.getMessage());
+        }
+    }
+
+    @Override
+    public void onPause() {
+        //  Make sure the AsyncTask (if it exists) is canceled)
+        if (mTmpTask != null) {
+            mTmpTask.cancel(true);
+            mTmpTask = null;
+        }
+
+        super.onPause();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View                    root;
+
+        root = inflater.inflate(R.layout.fragment_status, container, false);
+
+        mStatus = (EditText)root.findViewById(R.id.status_text);
+        mRemainText = (TextView)root.findViewById(R.id.status_remain);
         mRemainText.setText(Integer.toString(MAX_STATUS_LEN));
         mDefaultTextColor = mRemainText.getCurrentTextColor();
         mStatus.addTextChangedListener(this);
 
-        mSubmitBtn = (Button)findViewById(R.id.submit_status);
+        mSubmitBtn = (Button)root.findViewById(R.id.submit_status);
         mSubmitBtn.setOnClickListener(this);
 
-        mSettingsBtn = (Button)findViewById(R.id.btn_settings);
+        mSettingsBtn = (Button)root.findViewById(R.id.btn_settings);
         mSettingsBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startSettingsActivity();
             }
         });
+
+        return root;
     }
 
     private void startSettingsActivity() {
-        Intent settingsIntent = new Intent(this, SettingsActivity.class);
+        Intent settingsIntent = new Intent(getActivity(), SettingsActivity.class);
         startActivity(settingsIntent);
-    }
-
-    @Override
-    protected void onPause() {
-        //  Make sure the AsyncTask (if it exists) is canceled)
-        if (mTmpTask != null) {
-            mTmpTask.cancel(true);
-            mTmpTask = null;
-        }
-        super.onPause();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_status, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -130,22 +142,23 @@ public class StatusActivity extends Activity implements TextWatcher, View.OnClic
     }
 
     private class YambaSubmitterTask extends AsyncTask<String, Void, String> {
-        ProgressDialog          mProgDlg;
+        ProgressDialog mProgDlg;
 
         @Override
         protected String doInBackground(String... params) {
+            Activity act = getActivity();
             String status = params[0];
-            String result = StatusActivity.this.getString(R.string.success);
-            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(StatusActivity.this);
-            String tmp = StatusActivity.this.getString(R.string.username_key);
+            String result = act.getString(R.string.success);
+            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(act);
+            String tmp = act.getString(R.string.username_key);
             String username = prefs.getString(tmp, "");
-            tmp = StatusActivity.this.getString(R.string.password_key);
+            tmp = act.getString(R.string.password_key);
             String pw = prefs.getString(tmp, "");
-            tmp = StatusActivity.this.getString(R.string.api_uri_key);
+            tmp = act.getString(R.string.api_uri_key);
             String apiUri = prefs.getString(tmp, "");
             if ((pw.length() == 0) || (username.length() == 0) || (apiUri.length() == 0)) {
                 startSettingsActivity();
-                return StatusActivity.this.getString(R.string.fail);
+                return act.getString(R.string.fail);
             }
 
             YambaClient client = new YambaClient(username, pw, apiUri);
@@ -153,7 +166,7 @@ public class StatusActivity extends Activity implements TextWatcher, View.OnClic
                 client.postStatus(status);
             } catch (YambaClientException e) {
                 e.printStackTrace();
-                result = StatusActivity.this.getString(R.string.fail);
+                result = act.getString(R.string.fail);
             }
             return result;
         }
@@ -161,16 +174,17 @@ public class StatusActivity extends Activity implements TextWatcher, View.OnClic
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
-            String title = StatusActivity.this.getString(R.string.progress_title);
-            String msg = StatusActivity.this.getString(R.string.progress_msg);
-            mProgDlg = ProgressDialog.show(StatusActivity.this, title, msg, true);
+            Activity act = getActivity();
+            String title = act.getString(R.string.progress_title);
+            String msg = act.getString(R.string.progress_msg);
+            mProgDlg = ProgressDialog.show(act, title, msg, true);
         }
 
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
             mProgDlg.dismiss();
-            Toast.makeText(StatusActivity.this, s, Toast.LENGTH_LONG).show();
+            Toast.makeText(getActivity(), s, Toast.LENGTH_LONG).show();
             mStatus.setText("");
             mSubmitBtn.setEnabled(true);
             mTmpTask = null;
@@ -183,5 +197,4 @@ public class StatusActivity extends Activity implements TextWatcher, View.OnClic
             super.onCancelled();
         }
     }
-
 }
